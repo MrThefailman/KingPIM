08e0e6fa889b374f7c11c5bedb3c707fa5b3c72e
Timmie Bark <bati17tt@student.ju.se>
2018-10-17 14:06:11 +0200

    Added material design

---
+++ KingPIM/KingPIM.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KingPIM.Data
+{
+    class ApplicationDbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+    }
+}
